76465563e61c036a7f6b295062a10e599ffb200c
SKW1zy12 <lohmuhamatasiz@gmail.com>
2023-12-10 23:19:39 +0600

    hw

--- apps/settings/urls.py
+++ apps/settings/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from apps.settings.views import index, index2,index3,index4
+from apps.settings.views import index, index2,index3
 
 urlpatterns = [
-     path('', index,name='index'),
-     path('about/', index2,name='index2'),
-     path('blog/', index3,name='index3'),
-     path('index/', index4,name='index4')
+     path('index/', index,name='index'),
+     path('blog/', index2,name='index2'),
+     path('', index3,name='index3'),
 ]
--- apps/settings/views.py
+++ apps/settings/views.py
@@ -8,7 +8,4 @@ def index2(request):
     return render(request, 'blog.html', locals())
 
 def index3(request):
-    return render(request, 'index-kenburns.html', locals())
-
-def index4(request):
     return render(request, 'index-slideshow.html', locals())
